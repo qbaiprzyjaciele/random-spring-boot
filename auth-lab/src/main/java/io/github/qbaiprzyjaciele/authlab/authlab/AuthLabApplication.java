50cdb792c9b147a08744799bec84cc4637ff0134
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2019-02-26 11:21:33 +0100

    interesting

---
+++ auth-lab/src/main/java/io/github/qbaiprzyjaciele/authlab/authlab/AuthLabApplication.java
@@ -0,0 +1,13 @@
+package io.github.qbaiprzyjaciele.authlab.authlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLabApplication.class, args);
+	}
+
+}
