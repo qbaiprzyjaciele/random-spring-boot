1dc8256cb8f6394e8446ab6687ac138bf25b2d25
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2020-04-06 02:13:13 +0200

    websocket client

---
+++ websocketex/src/main/java/io/github/qbaiprzyjaciele/websocketex/SendMessageController.java
@@ -0,0 +1,30 @@
+package io.github.qbaiprzyjaciele.websocketex;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/message")
+public class SendMessageController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SendMessageController.class);
+
+    private final SimpMessagingTemplate template;
+
+    public SendMessageController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @GetMapping(value = "/send")
+    public String send(@RequestParam(value = "message") String message) {
+        LOG.info(message);
+        this.template.convertAndSend("/topic/chat", message);
+        return "sent";
+    }
+
+}
